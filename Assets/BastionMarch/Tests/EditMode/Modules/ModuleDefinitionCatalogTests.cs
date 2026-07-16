97f4de53adf07fc7df058fe943b6130cdbc535d1
VlaDOS <132541637+VladBorodin@users.noreply.github.com>
2026-07-16 17:44:33 +0400

    test: cover core module domain behavior

---
+++ Assets/BastionMarch/Tests/EditMode/Modules/ModuleDefinitionCatalogTests.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using BastionMarch.Simulation.Modules;
+using BastionMarch.Simulation.Modules.Catalog;
+using BastionMarch.Simulation.Modules.Features;
+using NUnit.Framework;
+
+namespace BastionMarch.Simulation.EditModeTests.Modules
+{
+    [TestFixture]
+    public sealed class ModuleDefinitionCatalogTests
+    {
+        [Test]
+        public void InitialCatalogContainsExpectedDefinitions()
+        {
+            ModuleDefinitionCatalog catalog =
+                InitialModuleDefinitions.CreateCatalog();
+
+            Assert.That(catalog.All.Count, Is.EqualTo(4));
+
+            Assert.That(
+                catalog.GetRequired(ModuleDefinitionIds.SmallMachineRoom),
+                Is.Not.Null);
+
+            Assert.That(
+                catalog.GetRequired(ModuleDefinitionIds.LargeMachineRoom),
+                Is.Not.Null);
+
+            Assert.That(
+                catalog.GetRequired(ModuleDefinitionIds.StandardRepairBay),
+                Is.Not.Null);
+
+            Assert.That(
+                catalog.GetRequired(ModuleDefinitionIds.StandardAmmoStorage),
+                Is.Not.Null);
+        }
+
+        [Test]
+        public void CatalogRejectsDuplicateDefinitionIds()
+        {
+            ModuleDefinition definition =
+                InitialModuleDefinitions
+                    .CreateCatalog()
+                    .GetRequired(ModuleDefinitionIds.SmallMachineRoom);
+
+            var duplicatedDefinitions = new[]
+            {
+                definition,
+                definition
+            };
+
+            Assert.Throws<ArgumentException>(
+                () => new ModuleDefinitionCatalog(duplicatedDefinitions));
+        }
+
+        [Test]
+        public void GetRequiredThrowsWhenDefinitionDoesNotExist()
+        {
+            ModuleDefinitionCatalog catalog =
+                InitialModuleDefinitions.CreateCatalog();
+
+            Assert.Throws<KeyNotFoundException>(
+                () => catalog.GetRequired("module.does-not-exist"));
+        }
+
+        [Test]
+        public void MachineRoomProvidesPropulsionFeature()
+        {
+            ModuleDefinitionCatalog catalog =
+                InitialModuleDefinitions.CreateCatalog();
+
+            ModuleDefinition definition =
+                catalog.GetRequired(ModuleDefinitionIds.SmallMachineRoom);
+
+            bool found = definition.TryGetFeature(
+                out PropulsionFeatureDefinition feature);
+
+            Assert.That(found, Is.True);
+            Assert.That(feature, Is.Not.Null);
+            Assert.That(feature.HorsePower, Is.EqualTo(1_200));
+            Assert.That(feature.FuelConsumptionPerTurn, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void RepairBayProvidesRepairSupportFeature()
+        {
+            ModuleDefinitionCatalog catalog =
+                InitialModuleDefinitions.CreateCatalog();
+
+            ModuleDefinition definition =
+                catalog.GetRequired(ModuleDefinitionIds.StandardRepairBay);
+
+            bool found = definition.TryGetFeature(
+                out RepairSupportFeatureDefinition feature);
+
+            Assert.That(found, Is.True);
+            Assert.That(feature, Is.Not.Null);
+            Assert.That(feature.RepairPointsPerTurn, Is.EqualTo(20));
+            Assert.That(feature.MaximumConcurrentJobs, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void AmmoStorageDoesNotProvidePropulsionFeature()
+        {
+            ModuleDefinitionCatalog catalog =
+                InitialModuleDefinitions.CreateCatalog();
+
+            ModuleDefinition definition =
+                catalog.GetRequired(ModuleDefinitionIds.StandardAmmoStorage);
+
+            bool found = definition.TryGetFeature(
+                out PropulsionFeatureDefinition feature);
+
+            Assert.That(found, Is.False);
+            Assert.That(feature, Is.Null);
+        }
+    }
+}
---
+++ Assets/BastionMarch/Tests/EditMode/Modules/ModuleInstanceTests.cs
@@ -0,0 +1,153 @@
+using System;
+using BastionMarch.Simulation.Modules;
+using BastionMarch.Simulation.Modules.Catalog;
+using NUnit.Framework;
+
+namespace BastionMarch.Simulation.EditModeTests.Modules
+{
+    [TestFixture]
+    public sealed class ModuleInstanceTests
+    {
+        private ModuleDefinition _definition;
+        private ModuleInstance _instance;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _definition = InitialModuleDefinitions
+                .CreateCatalog()
+                .GetRequired(ModuleDefinitionIds.SmallMachineRoom);
+
+            _instance = new ModuleInstance(
+                _definition,
+                new GridPosition(x: 0, deck: 0));
+        }
+
+        [Test]
+        public void NewModuleStartsFullyOperational()
+        {
+            Assert.That(
+                _instance.CurrentDurability,
+                Is.EqualTo(_definition.MaxDurability));
+
+            Assert.That(
+                _instance.TechnicalState,
+                Is.EqualTo(ModuleTechnicalState.Operational));
+
+            Assert.That(
+                _instance.ControlState,
+                Is.EqualTo(ModuleControlState.Friendly));
+        }
+
+        [Test]
+        public void DamageChangesTechnicalStateAtDefinitionThresholds()
+        {
+            _instance.ApplyDamage(40);
+
+            Assert.That(_instance.CurrentDurability, Is.EqualTo(60));
+
+            Assert.That(
+                _instance.TechnicalState,
+                Is.EqualTo(ModuleTechnicalState.Damaged));
+
+            _instance.ApplyDamage(35);
+
+            Assert.That(_instance.CurrentDurability, Is.EqualTo(25));
+
+            Assert.That(
+                _instance.TechnicalState,
+                Is.EqualTo(ModuleTechnicalState.Critical));
+
+            _instance.ApplyDamage(25);
+
+            Assert.That(_instance.CurrentDurability, Is.Zero);
+
+            Assert.That(
+                _instance.TechnicalState,
+                Is.EqualTo(ModuleTechnicalState.Destroyed));
+        }
+
+        [Test]
+        public void DamageCannotReduceDurabilityBelowZero()
+        {
+            _instance.ApplyDamage(1_000);
+
+            Assert.That(_instance.CurrentDurability, Is.Zero);
+
+            Assert.That(
+                _instance.TechnicalState,
+                Is.EqualTo(ModuleTechnicalState.Destroyed));
+        }
+
+        [Test]
+        public void RepairCannotExceedMaximumDurability()
+        {
+            _instance.ApplyDamage(70);
+            _instance.RestoreDurability(1_000);
+
+            Assert.That(
+                _instance.CurrentDurability,
+                Is.EqualTo(_definition.MaxDurability));
+
+            Assert.That(
+                _instance.TechnicalState,
+                Is.EqualTo(ModuleTechnicalState.Operational));
+        }
+
+        [Test]
+        public void ControlStateIsIndependentFromTechnicalState()
+        {
+            _instance.SetControlState(ModuleControlState.Occupied);
+            _instance.ApplyDamage(1_000);
+
+            Assert.That(
+                _instance.ControlState,
+                Is.EqualTo(ModuleControlState.Occupied));
+
+            Assert.That(
+                _instance.TechnicalState,
+                Is.EqualTo(ModuleTechnicalState.Destroyed));
+        }
+
+        [Test]
+        public void SameBrigadeCannotBeAssignedTwice()
+        {
+            Guid brigadeId = Guid.NewGuid();
+
+            bool firstAssignment =
+                _instance.AssignBrigade(brigadeId);
+
+            bool secondAssignment =
+                _instance.AssignBrigade(brigadeId);
+
+            Assert.That(firstAssignment, Is.True);
+            Assert.That(secondAssignment, Is.False);
+            Assert.That(_instance.AssignedBrigadeIds.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AssignedBrigadeCanBeRemoved()
+        {
+            Guid brigadeId = Guid.NewGuid();
+
+            _instance.AssignBrigade(brigadeId);
+
+            bool removed =
+                _instance.RemoveBrigade(brigadeId);
+
+            Assert.That(removed, Is.True);
+            Assert.That(_instance.AssignedBrigadeIds, Is.Empty);
+        }
+
+        [Test]
+        public void ModulePositionCanBeChanged()
+        {
+            var newPosition = new GridPosition(x: 4, deck: 2);
+
+            _instance.SetPosition(newPosition);
+
+            Assert.That(_instance.Position.X, Is.EqualTo(4));
+            Assert.That(_instance.Position.Deck, Is.EqualTo(2));
+        }
+    }
+}
